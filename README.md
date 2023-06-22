37f82a681eb91e219444df9d4e2b71039532064e
zukashix <zukashixx@gmail.com>
2023-06-22 18:01:35 +0530

    Bootstrapper completed. Beautified README

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# bscraft
-read this with fine precision <br>
-you can only use python 3.10.6 64bit <br>
-you will need to install modules `PyQt5` and `requests` <br>
-you need specific versions of this module. the command will be <br>
-`python -m pip install PyQt5==5.15.6 PyQt5-sip==12.9.1`
+# BSCraft Launcher
+
+Source for BSCraft 3.0 Launcher & Bootstrapper <br>
+
+## Progress
+
+- Launcher bootstrapper ready. ((1))
+- Bootstrapper development-platform test (ubuntu/aarch64) = Success ((2))
+- Bootstrapper target-platform test (windows/amd64) = Pending ((3))
+- Launcher bootstrapper platform-build release (exe/windows/amd64) = Waiting for (3) ((4))
+- Begin work on main launcher = TBA ((5))
+
+## Development Target
+
+- Windows OS (7 and up) are the target platforms.
+- Imported/Native code is recommended cross platform or atleast support (linux/aarch64).
+- Build target is amd64.
+- Python 3.10.6 64-bit must be used.
+- PyQt5 v5.15.6 must be used.
+- PyInstaller to build executable must be used.
+
+## Execution (Bootstrapper)
+
+- Ensure Python 3.10.6 64-bit is installed.
+- `git clone` OR download this repository and set repo root to working directory.
+- Install modules. `python -m pip install -r requirements.txt`
+- Run the script. `python ./bootstrapper/main.py`
+- If background image/fonts do not load, check working directory (must be the root folder, not the `bootstrapper` folder.) OR modify code to point to the files correctly.
--- bootstrapper/main.py
+++ bootstrapper/main.py
@@ -1,36 +1,32 @@
 import sys
-import time
 import os
 import json
 import requests
 import subprocess
 import platform
 
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QDesktopWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QDesktopWidget, QMessageBox
 from PyQt5.QtGui import QPixmap, QFont, QFontDatabase
 from PyQt5.QtCore import Qt, QEvent
 
 plat = platform.system().lower()
 if 'win' in plat:
-    APPDATA = os.getenv("APPDATA") # windows
+    APPDATA = os.getenv("APPDATA").replace("\\", "/") + '/' # windows
 else:
     APPDATA = os.getenv("HOME") + '/' # linux
 
 
 class BackendUtilities:
-    def checkInternet() -> int:
+    def checkInternet() -> bool:
         headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
         }
         try:
-            requests.get('https://example.com', timeout=10, headers=headers)
-            try:
-                requests.get('https://braxtonelmer.com', timeout=10, headers=headers)
-                return 0
-            except:
-                return 1
+            requests.get('https://updater.braxtonelmer.com/', timeout=10, headers=headers)
+            return True
         except:
-            return 2
+            return False
+
 
     def downloadFile(file_url, file_loc) -> bool:
         try:
@@ -38,7 +34,7 @@ class BackendUtilities:
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
             }
             r = requests.get(file_url, stream = True, headers=headers, verify=False)
-            with open(file_loc, 'w') as ufile:
+            with open(file_loc, 'wb') as ufile:
                 for chunk in r.iter_content(chunk_size=1024):
         
                 # Writing one chunk at a time to file (No excessive memory usage on large-size files)
@@ -58,22 +54,67 @@ class BackendInitiliazer():
             self.ALR_INSTALLED = json.load(open(APPDATA + '.bscraft/validation.json', 'r'))["bootstrapper"]["mainExecValid"]
         except (FileNotFoundError, KeyError):
             self.ALR_INSTALLED = False
+
+        self.utils = BackendUtilities
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0',
+            }
     
+
     def isInstalled(self) -> bool:
         return self.ALR_INSTALLED
     
+
+    def getMbSize(self) -> str:
+        fileUrl = requests.get("https://updater.braxtonelmer.com/BSCraft/bootstrapper_data.json", headers=self.headers).json()['latest_url']
+        fileByteSize = int(requests.head(fileUrl, headers=self.headers).headers['Content-Length'])
+        return str(int(fileByteSize / 1048576))
+
     def startProcess(self):
         if 'win' in plat:
             args = ['start', '/B',  APPDATA + '.bscraft/launcher.exe']
         else:
-            args = ['nohup', 'firefox', '&']
+            args = ['nohup', 'firefox']
         
         subprocess.Popen(args)
+
+
+    def install(self):
+        repoData = requests.get("https://updater.braxtonelmer.com/BSCraft/bootstrapper_data.json", headers=self.headers).json()
+
+        try:
+            os.makedirs(APPDATA + '.bscraft')
+        except FileExistsError:
+            pass
+
+        self.utils.downloadFile(repoData['latest_url'], APPDATA + '.bscraft/launcher.exe')
+
+        validityJsonData = {
+            'bootstrapper': {
+                'mainExecValid': True,
+                'localVersion': repoData['latest_version']
+            }
+        }
+
+        json.dump(validityJsonData, open(APPDATA + '.bscraft/validation.json', 'w'))
+
+
+    def checkForUpdate(self) -> bool:
+        repoData = requests.get("https://updater.braxtonelmer.com/BSCraft/bootstrapper_data.json", headers=self.headers).json()
+        localData = json.load(open(APPDATA + '.bscraft/validation.json', 'r'))
+
+        if localData['bootstrapper']['localVersion'] == repoData['latest_version']:
+            return False
+        else:
+            return True
+
     
 
 class DisplayGUI(QMainWindow):
-    def __init__(self):
+    def __init__(self, statusText: str, mainInit: BackendInitiliazer):
         super(DisplayGUI, self).__init__()
+        self.statusText = statusText
+        self.mainInit = mainInit
 
         # Set window properties
         self.setWindowTitle("BSCL Updater")
@@ -85,13 +126,13 @@ class DisplayGUI(QMainWindow):
         # Set background image
         self.background_label = QLabel(self)
         self.background_label.setGeometry(0, 0, 800, 400)
-        self.background_image = QPixmap("bootstrapper/bgimg.jpg")
+        self.background_image = QPixmap("bootstrapper/resources/bgimg.jpg")
         self.background_label.setPixmap(self.background_image)
 
         # Set bscl text
         self.text_label = QLabel(self)
         self.text_label.setGeometry(40, 50, 450, 80)
-        QFontDatabase.addApplicationFont("bootstrapper/mcfont.ttf")
+        QFontDatabase.addApplicationFont("bootstrapper/resources/mcfont.ttf")
         self.text_label.setFont(QFont("MineCrafter 3", 40))
         self.text_label.setText("BSCraft")
         self.text_label.setStyleSheet("color: white")
@@ -99,10 +140,11 @@ class DisplayGUI(QMainWindow):
         # set status text
         self.text_label_2 = QLabel(self)
         self.text_label_2.setGeometry(40, 340, 350, 40)
-        QFontDatabase.addApplicationFont("bootstrapper/Minecraftia.ttf")
+        QFontDatabase.addApplicationFont("bootstrapper/resources/Minecraftia.ttf")
         self.text_label_2.setFont(QFont("Minecraftia", 15))
-        self.text_label_2.setText("Checking For Updates")
+        self.text_label_2.setText(self.statusText)
         self.text_label_2.setStyleSheet("color: lightgreen")
+
 
     def _centerWindow(self):
         # function to help center the window on any resolution                      
@@ -111,19 +153,54 @@ class DisplayGUI(QMainWindow):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
 
+
     def postDisplay(self):
-        print('gui should be displayed. time 0s')
-        time.sleep(5)
-        print('time 5s')
+        self.mainInit.install()
+        self.mainInit.startProcess()
+        exit(0)
+
 
     def event(self, event):
         if event.type() == QEvent.InputMethodQuery:
             self.postDisplay()
         return super(DisplayGUI, self).event(event)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = DisplayGUI()
-    window.show()
 
-    sys.exit(app.exec_())
+if __name__ == "__main__":
+    # init required data
+    app = QApplication(sys.argv)
+    iNetC = BackendUtilities.checkInternet()
+    mainInit = BackendInitiliazer()
+    isInst = mainInit.isInstalled()
+    
+    # check internet conditions and installed files
+    if not iNetC:
+        if not isInst:
+            QMessageBox.critical(None, "BSCraft Launcher", "No internet or server down. Quitting because files are missing.")
+            sys.exit(1)
+        else:
+            QMessageBox.warning(None, "BSCraft Launcher", "Could not check for updates. No internet or server down. Press OK to continue launching.")
+            mainInit.startProcess()
+            sys.exit(0) 
+
+    else:
+        if isInst:
+            if mainInit.checkForUpdate():
+                filesize = mainInit.getMbSize()
+                statustext = "Updating... ({} MB)".format(filesize)
+
+                window = DisplayGUI(statustext, mainInit)
+                window.show()
+                sys.exit(app.exec_())
+
+            mainInit.startProcess()
+            exit(0)
+
+        else:
+            filesize = mainInit.getMbSize()
+            statustext = "Installing... ({} MB)".format(filesize)
+
+            window = DisplayGUI(statustext, mainInit)
+            window.show()
+            sys.exit(app.exec_())
+            
